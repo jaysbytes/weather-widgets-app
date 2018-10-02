b89c0104b9951c0122fb62228fe383d89667853f
Jakubscode <jakub_mazurek96@onet.pl>
2018-10-02 19:08:07 +0200

    feat: redux store persistence

--- app/src/store.js
+++ app/src/store.js
@@ -1,13 +1,38 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import debounce from 'debounce'
 
 import userReducer from './services/user.service/reducer'
+
+const storeKeyName = 'reduxStore'
+
+const loadState = () => {
+  try {
+    const persistedState = localStorage.getItem(storeKeyName)
+    return JSON.parse(persistedState) || undefined
+  }
+  catch(e) {
+    return undefined
+  }
+}
+const saveState = (state) => {
+  try {
+    localStorage.setItem(storeKeyName, JSON.stringify(state))
+  }
+  catch(e) {
+    return undefined
+  }
+}
 
 const combinedReducers = combineReducers({
   userState : userReducer,
 })
 
-const store = createStore(combinedReducers, applyMiddleware(thunk))
+const store = createStore(combinedReducers, loadState(), applyMiddleware(thunk))
 
+store.subscribe(debounce(() => {
+  const state = store.getState()
+  saveState(state)
+}, 1000))
 
 export default store
